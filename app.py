d94b5f8dc13f220194a8ab692c414351a531897f
Kyle Lin <klin12@stuy.edu>
2018-05-09 13:27:22 -0400

    Initial Commit

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, url_for, redirect, session
+import sqlite3
+
+app = Flask(__name__)
+
+@app.route('/', methods=["GET", "POST"])
+def index():
+    if request.method == "POST":
+        redirect(url_for("index"))
+    
+    return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run()
