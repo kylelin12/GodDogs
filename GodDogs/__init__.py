c55d9e664e7666116b01c863309c70ac7b9161a4
Kyle Lin <klin12@stuy.edu>
2018-05-22 10:15:26 -0400

    Added routes for most things. Added fancy buttons to profile page and added examples + placeholders for each page.

--- GodDogs/__init__.py
+++ GodDogs/__init__.py
@@ -5,16 +5,35 @@ import sqlite3
 
 app = Flask(__name__)
 
-@app.route('/', methods=["GET", "POST"])
+# Index page
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    if request.method == "POST":
-        redirect(url_for("index"))
-    
+    if request.method == 'POST':
+        redirect(url_for('index'))
     return render_template('index.html')
 
-@app.route('/profile', methods=["GET", "POST"])
-def about():
-    return render_template('profile.html', name="DANIEL")
+# Global chatroom
+@app.route('/gchat', methods=['GET', 'POST'])
+def gchat():
+    if request.method == 'POST':
+        redirect(url_for('gchat'))
+    return render_template('gchat.html')
+
+# Shows your profile if from top right
+# Shows any user's profile if their name is selected
+@app.route('/profile', methods=['GET', 'POST'])
+def profile():
+    if request.method == 'POST':
+        redirect(url_for('profile'))
+    return render_template('profile.html', name='DANIEL')
+
+# Shows your list of friends if logged in
+# If not logged in, redirect to login
+@app.route('/friendslist', methods=['GET', 'POST'])
+def friendslist():
+    if request.method == 'POST':
+        redirect(url_for('friendslist'))
+    return render_template('friendslist.html', name='DANIEL')
 
 if __name__ == '__main__':
     app.debug = True
